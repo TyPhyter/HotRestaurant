443500a7310ab82604e11894c39f0de687460f26
Grace Ho <rainczech@gmail.com>
2018-04-25 19:36:23 -0500

    routes

---
+++ server/routes/routes.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "www/index.html"));
+  });
+
+app.get("/reservation", function(req, res){
+    res.sendfile(path.join(__dirname, "www/reservation.html"));
+});
+
+app.get("/tables", function(req, res){
+    res.sendfile(path.join(__dirname, "www/tables.html"));
+});
+
+// port listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+// show all tables
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+  });
+
+
+var tables = [
+    {
+      name: "",
+      phone: "",
+      email: "",
+      uniqueId: "",
+    },
+];
+
+
+
+
