1481ef278afbe950bf025508b9bc8e1eb763f5cc
TyPhyter <tylerjenkins08@gmail.com>
2018-04-25 21:01:28 -0500

    path changes

--- server/www/assets/javascript/index.js
+++ server/www/assets/javascript/index.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
-    $a = $('a');
-
-    $a.on('click', function(){
-        //make a GET request for the document in the hash
-        $this = $(this);
-        var page = '/' + $this.prop('hash').split('#').join('');
-        console.log(page);
-        //$.get(page)
-    });
+ 
 
     
 });
